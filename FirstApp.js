fa1a0aa7c9eca8da4422dd8ee84bb382f57d6525
shubh532 <shubhammamahulkar2000>
2023-07-16 18:37:17 +0530

    FIle Reading Mini Project

--- FirstApp.js
+++ FirstApp.js
@@ -1,24 +1,42 @@
-//My First Server
 const http = require('http');
+const fs = require("fs");
+const { error } = require('console');
+
 const server = http.createServer((req, res) => {
-  const url=req.url
-  if(url==="/home"){
-  res.write("<h1>Welcome TO my Home</h1>");
-  res.end();
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt",{encoding:"utf-8"},(err,data)=>{
+      if (err){
+        console.log(err)
+      }
+      console.log(data,"DAT")
+      res.write("<html>")
+      res.write("<head><title>Enter Message</title></head>")
+      res.write(`<body><h1>${data}</h1><form action='/message' method='POST'><input type='text' name='message'></input><button type='submit'>Submit</button></form></body>`)
+      res.write("</html>")
+      return res.end()
+    })
+  }
+  if (url === "/message" && method === "POST") {
+    const body = []
+    req.on("data", (chunk) => {
+      console.log(chunk)
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString()
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+      res.write(`<head><title>${message}</title></head>`)
+    })
+    res.statusCode = 302
+    res.setHeader("Location", '/')
+    return res.end();
+  }
+  // res.setHeader("Content-Type","text-html")
+})
 
-}else if (url==="/about"){
-  res.write("<h1>Welcome TO my About Page </h1>");
-  res.end();
-
-}else if (url==="/node"){
-  res.write("<h1>Welcome TO my NODE JS PROJECT </h1>");
-  res.end();
-}else{
-  res.write("<h1>Welcome Shubham </h1>");
-  res.end();
-}
-  
-});
 
 server.listen(4000, 'localhost', () => {
   console.log('Server is running on http://localhost:4000');
