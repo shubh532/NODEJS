6bc0a670a6a8aac7968f7493a084fc7ab7cb50a3
shubh532 <shubhammamahulkar2000>
2023-07-16 13:44:45 +0530

    navigating through different urls

--- FirstApp.js
+++ FirstApp.js
@@ -1,8 +1,23 @@
 //My First Server
 const http = require('http');
 const server = http.createServer((req, res) => {
+  const url=req.url
+  if(url==="/home"){
+  res.write("<h1>Welcome TO my Home</h1>");
+  res.end();
 
-  res.end("Shubham Mahulkar");
+}else if (url==="/about"){
+  res.write("<h1>Welcome TO my About Page </h1>");
+  res.end();
+
+}else if (url==="/node"){
+  res.write("<h1>Welcome TO my NODE JS PROJECT </h1>");
+  res.end();
+}else{
+  res.write("<h1>Welcome Shubham </h1>");
+  res.end();
+}
+  
 });
 
 server.listen(4000, 'localhost', () => {
