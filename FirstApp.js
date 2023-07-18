37541ecb9caf4120d8eccb70d07c8c1cd1dbde1e
shubh532 <shubhammamahulkar2000>
2023-07-18 11:47:21 +0530

    Intro  to middleware and body parsing

--- FirstApp.js
+++ FirstApp.js
@@ -1,17 +1,23 @@
-const http = require('http');
+const bodyParser = require("body-parser")
 const express=require("express")
 const app=express()
 
-app.use((req, res, next)=>{
-  res.send({Key:"value"})
-  console.log("Hi Iam Middleware")
-  next()
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use("/add-product",(req, res, next)=>{
+  res.send("<html><body><form action=/product method='POST'><input type='text' name='Name'/><input type='number' name='Size'/><button  type='submit'>Submit</button></form></body></html>")
 })
-app.use((req, res, next)=>{
-  
-  console.log("Hi I am another Middleware")
+
+app.use("/product",(req,res,next)=>{
+  res.redirect("/")
+  console.log(req.body)
+})
+
+app.use("/",(req,res,next)=>{
+  res.send("<html> <body> <h1>WelCome To Express Js</h1></body></html>")
 })
 
 app.listen(4000, 'localhost', () => {
   console.log('Server is running on http://localhost:4000');
 })
+
